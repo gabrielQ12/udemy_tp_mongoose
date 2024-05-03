a17b2ee701b46d18417d08937e01f7d2ba6f4670
gabrielq12 <gabriel.quinette1@gmail.com>
2024-05-03 16:12:40 +0200

    mise en place mongoose: DONE

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require ("express");
-
+const mongoose = require ("mongoose");
 const app = express();
-
 const morgan = require ("./middlewares/infoRequete");
-
 const bodyParser = require ("./middlewares/parserDonnées");
 
 app.use(bodyParser);
 
 // morgan permet de voir les requêtes http dans la console
 app.use(morgan);
+
+require("dotenv").config();
+
+
+// connexion à la BDD mongoDB
+mongoose
+    .connect (process.env.LIEN, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connecté à la BDD"))
+    .catch(() => console.log("Erreur de connection"));
 
 app.get("",(req,res) => {
     res.status(200)
